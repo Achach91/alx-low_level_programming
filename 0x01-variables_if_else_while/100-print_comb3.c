570b9320c1b1bc0c083ac745db49c80754881402
Achach <adhiambojuliu5@gmail.com>
2022-06-03 13:30:56 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,19 +11,19 @@ int main(void)
 	int ones;
 
 		for (tens = 0; tens <= 9; tens++)
-		 {
+		{
 		for (ones = 0; ones <= 9; ones++)
 	{
-	 putchar(tens + '0');
-	 putchar(ones + '0');
+	putchar(tens + '0');
+	putchar(ones + '0');
 
-	 if (!(tens == 9 && ones == 9))
-	   {
-	     putchar(',');
-	     putchar(' ');
-	   }
-	   }
+	if (!(tens == 9 && ones == 9))
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+		}
 putchar('\n');
 
 return (0);
