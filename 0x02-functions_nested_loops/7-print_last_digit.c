7361715625ab71c58c990991b5bcf7278febd73b
Achach <adhiambojuliu5@gmail.com>
2022-06-08 14:21:22 -0700

    7-print-last-digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,9 +2,8 @@
 
 /**
  * print _last_digit - prints the last digit
- * of a number
  *
- * Description: print the last digit of a number.
+ * Description:print the last digit of a number.
  *
  * @n: input number as an integer.
  *
@@ -12,7 +11,7 @@
  */
 int print_last_digit(int n)
 {
-	int (1);
+	int 1;
 
 	1 = n % 10;
 	if (1 < 0)
